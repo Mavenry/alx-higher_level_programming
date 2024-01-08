f23c8e09dbb1317b41e788fff6b9fa47a2a7ce77
Mavenry <sundaybukola10@gmail.com>
2024-01-08 11:39:51 -0800

    element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx):
     if idx < 0:
         return None
-    elif idx not in my_list:
+    elif idx >=len(my_list):
        return None
     else:
        return my_list[idx]
