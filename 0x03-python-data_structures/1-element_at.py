d01fcbc66c9541a945c32eb1ffa9c62bbbc240ac
Mavenry <sundaybukola10@gmail.com>
2024-01-08 07:53:42 -0800

    element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx not in my_list:
+       return None
+   else:
+       for idx in my_list:
+           print("{:d}".format(my_list))
