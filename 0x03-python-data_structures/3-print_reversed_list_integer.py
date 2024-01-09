66eeea9ba55c5d075e7056051b63db6a1f375877
Mavenry <sundaybukola10@gmail.com>
2024-01-09 07:04:24 -0800

    reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,4 +5,4 @@ def print_reversed_list_integer(my_list=[]):
     else:
         reverse_list = my_list[::-1]
         for i in reverse_list:
-            print("{:d}" .format(reverse_list[i]))
+            print("{:d}" .format(i))
