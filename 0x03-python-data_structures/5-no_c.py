1dba2bbb4e404fd8e84aac07e576efd89d57b441
Mavenry <sundaybukola10@gmail.com>
2024-01-09 09:30:18 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    word = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            word = word + my_string[i]
-    return (word)
+    for i in my_string:
+        if i = 'c' or i = 'C':
+            continue
+        new_string[i] = my_string[i]
+        return new_string
