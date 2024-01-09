d02d58c92683f992f67f7b64b4cc0f832d39b467
Mavenry <sundaybukola10@gmail.com>
2024-01-09 10:56:44 -0800

    add tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    lenght_a = len(tuple_a)
+    lenght_b = len(tuple_b)
+    if lenght_a < 2:
+        if lenght_a == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+
+    if lenght_b < 2:
+        if lenght_b == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    added_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return added_tuple
