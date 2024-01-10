f35a2ba4614067f343d1316563f08c07962f39f0
Mavenry <sundaybukola10@gmail.com>
2024-01-10 12:10:47 -0800

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -9,4 +9,4 @@ def square_matrix_simple(matrix=[]):
             for element in row:
                 square_row.append(element **2)
             new_matrix.append(square_row)
-    return(new_matrix)
+        return(new_matrix)
