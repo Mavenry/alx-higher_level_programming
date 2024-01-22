09c7875aed7336949982109920388a48dded1415
Mavenry <sundaybukola1@gmail.com>
2024-01-22 08:06:40 -0800

    safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print(my_list[i])
+    except IndexError:
+        pass
+    finally:
+        return min(x, len(my_list))
