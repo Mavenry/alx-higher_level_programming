e4f8c63b559565ba67a75d1b19d9252f722575f0
Mavenry <sundaybukola1@gmail.com>
2024-01-22 08:24:50 -0800

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i], end=' ')
-            count =count + 1
+            print(my_list[i])
+            count = +1
     except IndexError:
-        pass
+        break
     finally:
-        print()
         return count
