481a420f1f8a6d42b03784e714eb2c6efb90bb20
Mavenry <sundaybukola1@gmail.com>
2024-01-25 04:57:25 -0800

    sqaure

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-'''
-class - square, it is a class to find the square of objects
-
-method - method multiplies the variebles given using the formulas provided
-'''
+"""
+Define a class square"""
 class square:
     pass
