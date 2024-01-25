b37e6aa04b03ee55ddae3b70079006ec27e4e6c0
Mavenry <sundaybukola1@gmail.com>
2024-01-25 12:53:57 -0800

    square1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,8 @@
 defines class Square
 """
 class Square:
+    """setting up a class"""
     def __init__ (self, size):
+        """initializing the class
+           size is the size of the square"""
         self.__size = size
