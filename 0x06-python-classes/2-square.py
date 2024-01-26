f8e2e98869657f6b7ec921529f88219ec741cd0c
Mavenry <sundaybukola1@gmail.com>
2024-01-26 07:34:29 -0800

    square2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """Defines a class"""
+
 class Square:
     """class representation"""
 
     def __init__(self, size = 0):
         """Initializing the Square class
         Args: size - is the size of the square aad it is initiated to 0
+        raises
+        TypeError: must be int
+        ValueError: must be greater or equal to 0
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
