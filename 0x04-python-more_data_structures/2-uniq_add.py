1f1a655c84ee68dd1a72f2b2a498743f931b3f80
Mavenry <sundaybukola1@gmail.com>
2024-03-19 09:30:24 -0700

    uniq_add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     total = 0
-    for i in my_list:
-        if my_list.count(i) == 1:
+    for i in range(len(my_list)):
+        if my_list.count(my_list[i]) == 1:
             total += i
     return total
