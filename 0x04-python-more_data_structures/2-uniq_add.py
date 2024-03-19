046e64c43ae8cb2c7c2b7704c2dd49c54f5cd193
Mavenry <sundaybukola1@gmail.com>
2024-03-19 09:44:28 -0700

    add uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    unique_numbers = set()
     total = 0
-    for i in range(len(my_list)):
-        if my_list.count(my_list[i]) == 1:
-            total += my_list[i]
+    
+    for num in my_list:
+        if my_list.count(num) == 1 and num not in unique_numbers:
+            total += num
+            unique_numbers.add(num)
+    
     return total
