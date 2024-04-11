ddce9a3017cbd211a6128e1c6c9c7550e5ad81a3
Mavenry <sundaybukola@gmail.com>
2024-04-11 13:15:16 -0700

    property rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,14 +21,13 @@
                 return self.__width
             
             @width.setter
-            def width(self,value):
+            def width(self, value):
                 """ setting the width attribute """
                 if not isinstance(value, int):
                     raise TypeError("width must be an integer")
                 if width < 0:
                     raise ValueError("width must be >= 0")
-                else:
-                    self.__width = value
+                self.__width = value
 
            @property
            def height(self):
@@ -42,6 +41,5 @@
                    raise TypeError("height must be an integer")
                if height < 0:
                    raise ValueError("height must be >= 0")
-               else:
-                   self.__height = value
+               self.__height = value
 
