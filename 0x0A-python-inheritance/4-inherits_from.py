9384c76771c60f9d63d9f6034b688566270cd119
Mavenry <sundaybukola@gmail.com>
2024-04-15 15:11:50 -0700

    inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""module that checks the object is and instance of a super class"""
+"""module that checks the object is and instance of a a_class"""
 
 
 def is_kind_of_class(obj, a_class):
-    """Return True if obj is an instance of super class otherwise False"""
+    """Return True if obj is an instance of a_class otherwise False"""
     return isinstance(obj, a_class)
