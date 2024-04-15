a867fdcdd00e55beaf3855ad8973abcef22f2e1a
Mavenry <sundaybukola@gmail.com>
2024-04-15 15:31:33 -0700

    inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""module to check if the object is an instance of a class"""
+"""module to check if the object is an instance of a_class"""
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a_class otherwise False"""
-    return isinstance(obj, a_class)
+    """Return True if obj is an instance of a_class otherwise return False"""
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
