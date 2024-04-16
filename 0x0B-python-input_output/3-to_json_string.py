45a2c955a65030f08188f0b53bd39cf01681be03
Mavenry <sundaybukola@gmail.com>
2024-04-16 14:21:23 -0700

    json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module returns JSON representation of an object"""
+
+
+def to_json_string(my_obj):
+    """Returns JSON representation of an object"""
+    return json.dumps(my_obj)
+   
