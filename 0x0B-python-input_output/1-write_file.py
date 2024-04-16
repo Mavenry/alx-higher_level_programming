990540865c90506c2f9c3505ad0657866650eb7e
Mavenry <sundaybukola@gmail.com>
2024-04-16 13:03:41 -0700

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module that writes a string to a text file (UTF8) and returns the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """function that writes to a text file"""
+    with open(filename, 'w', encoding='utf-8'):
+        f.write(text)
+        return len(text)
