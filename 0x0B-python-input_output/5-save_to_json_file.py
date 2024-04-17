5dcfad3623aab2988f327c573386bbae7b6cbe80
Mavenry <sundaybukola@gmail.com>
2024-04-17 06:26:51 -0700

    save to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a JSON to text file function"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """This funtion writes an object to a text file using JSON representation"""
+    json_string = json.dumps(my_obj)
+    with open(filename,"w", encoding="utf-8") as f:
+        f.write(json_string)
