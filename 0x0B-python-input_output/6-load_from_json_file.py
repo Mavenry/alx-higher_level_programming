d9d762dadbd650ada60b357b2c7f6a726e0637ab
Mavenry <sundaybukola@gmail.com>
2024-04-17 07:46:28 -0700

    load from json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/urs/bin/python3
+"""Module that creates a file from JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates a file from json file"""
+    with open(filename, "w") as f:
+        json.load(f)
