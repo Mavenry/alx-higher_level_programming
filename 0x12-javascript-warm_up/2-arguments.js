17e509016babd20aed8a465176504e538641c09c
Mavenry <sundaybukola10@gmail.com>
2024-06-21 21:23:08 -0700

    added solutions

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ const args = process.argv;
 const argCount = args.length - 2;
 
 if (argCount === 0){
-	console.log("No Argument");
+	console.log('No Argument');
 } else if (argCount === 1) {
-	console.log("Argument found");
-} else if (argCount > 1) {
-	console.log("Arguments found");
+	console.log('Argument found');
+} else (argCount > 1) {
+	console.log('Arguments found');
 }
