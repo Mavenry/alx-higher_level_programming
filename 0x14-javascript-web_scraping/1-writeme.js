149a03d3e46cf3939ba666d0b7d8c6bd734466ca
Mavenry <sundaybukola@gmail.com>
2024-07-31 14:04:02 -0700

    solution added to task

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filename = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filename,content, utf-8, (error) => {
+fs.writeFile(filename, content, 'utf-8', (error) => {
 	if (error) {
 		console.log(error);
 	}
