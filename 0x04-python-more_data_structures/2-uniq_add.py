c4f7a111deb28f1df025ac2559f075bfda6e9abe
Mavenry <sundaybukola10@gmail.com>
2024-09-23 14:47:26 -0700

    solution added

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_numbers = set()
-    total = 0
-    
-    for num in my_list:
-        if my_list.count(num) == 1 and num not in unique_numbers:
-            total += num
-            unique_numbers.add(num)
-    
-    return total
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
